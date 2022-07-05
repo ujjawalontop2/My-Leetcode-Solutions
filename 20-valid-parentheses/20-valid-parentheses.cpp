d37c9c6cd1efa1d4f4c000cb7540793272e6f85e
Ujjawal Raj <102991860+ujjawalontop2@users.noreply.github.com>
2022-07-05 23:29:33 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (50.92%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+   bool check(char a,char b){
+        return a=='(' && b==')' ||a=='{' && b=='}' ||a=='[' && b==']' ;
+    }
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+                if(check(st.top(),s[i])==false) return false;
+                else{
+                    st.pop();
+                }
+            }
+        }
+        return st.empty();
+    }
+};
