e63cf7f976eb9a86be9f9e77fade237e89b29aba
Ujjawal Raj <102991860+ujjawalontop2@users.noreply.github.com>
2022-07-25 02:46:01 +0530

    Time: 536 ms (28.69%), Space: 35.8 MB (41.20%) - LeetHub

---
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int dp[601][101][101];
+    int solve(int i, vector<pair<int,int>> &a, int m, int n) {
+        if(i == a.size()) return 0;
+        if(dp[i][m][n] != -1) return dp[i][m][n];
+        int notake = solve(i+1,a,m,n);
+        int take = -1;
+        int c_zero = a[i].first, c_one = a[i].second;
+        if(c_zero <= m and c_one <= n) {
+            take = 1 + solve(i+1,a,m-c_zero,n-c_one);
+        }
+        return dp[i][m][n] = max(take, notake);
+    }
+    
+    int findMaxForm(vector<string>& s, int m, int n) {
+        int size_s = s.size();
+        int k = 0;
+        vector<pair<int,int>> a(size_s);
+        memset(dp,-1,sizeof(dp));
+        for(auto &i : s) {
+            int c = 0;
+            for(auto &j : i) {
+                if(j == '0') c++;
+            }
+            a[k++] = {c,abs((int)i.size()-c)};
+        }
+        return solve(0,a,m,n);
+    }
+};
